06ddfecaa4bdb6a951e653d1ce4d843e5bdcd2ae
Praise Idowu <ifeoluwapraise02@gmail.com>
2023-02-14 04:09:39 -0600

    Created the content models

--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User 
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 class Subject(models.Model):
     title = models.CharField(max_length=200)
@@ -32,3 +34,45 @@ class Module(models.Model):
     
     def __str__(self):
         return self.title
+
+
+class Content(models.Model):
+    module = models.ForeignKey(Module, related_name='content', on_delete=models.CASCADE) 
+    content_type = models.ForeignKey(ContentType, 
+                                    on_delete=models.CASCADE,
+                                    limit_choices_to={'model__in': (
+                                        'text',
+                                        'video',
+                                        'image',
+                                        'file'
+                                    )}) 
+    object_id = models.PositiveIntegerField()  #to store pk of related obj
+    item = GenericForeignKey('content_type', 'object_id')
+    
+    
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User, related_name='%(class)s_related', on_delete=models.CASCADE)  #% reps a placeholder
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        abstract = True
+        
+    def __str__(self):
+        return self.title
+    
+class Text(ItemBase):
+    content = models.TextField()
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files')  # to store files such as pdfs
+
+class Image(ItemBase):
+    file = models.FileField(upload_to='files')
+
+class Video(ItemBase):
+    url = models.URLField()   #provide a video URL in order to embed it
+    
+#A database table will be created for the Text, File, Image, and Video models, respectively. There will be 
+#no database table associated with the ItemBase model since it is an abstract model.
