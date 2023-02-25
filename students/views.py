c104902d431f71a6b26e73d56ca0f68c2d9f3be0
Praise Idowu <ifeoluwapraise02@gmail.com>
2023-02-25 12:35:38 +0100

    Created student registration view

---
+++ students/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+
+
+class StudentRegistrationView(CreateView):
+    template_name = 'students/student/registration.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('student_course_list')  #redirect the user after successful submission
+    
+    def form_valid(self, form):  #executed when valid form data has been posted: override it to log user in when dey sign up
+        result = super().form_valid(form)
+        cd = form.cleaned_data
+        user = authenticate(username=cd['username'], password=cd['password1'])
+        login(self.request, user)
+        return result
