629d59b10e766717d9040ce7564049b6956805a1
Praise Idowu <ifeoluwapraise02@gmail.com>
2023-02-28 12:51:15 +0100

    Caching based on dynamic data

--- educa/settings.py
+++ educa/settings.py
@@ -47,9 +47,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
